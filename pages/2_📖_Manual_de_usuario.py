f0f1788a046666a009e1f3e1ced3f078740f0c0f
Camilo Rozo <ce.rozob@uniandes.edu.co>
2024-05-21 00:55:22 -0500

    done2

--- pages/2_📖_Manual_de_usuario.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-
-
-st.set_page_config("Manual de usuario",page_icon="📖")
-
-st.title("Manual de usuario")
-st.write("Aquí encontrarás una guía de cómo utilizar la aplicación")
